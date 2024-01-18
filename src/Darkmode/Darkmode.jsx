d241215e38f0a1f4df6f5d4abfa8b2c306f7665d
Mohit Parmar <100152343+mohitparmar1@users.noreply.github.com>
2024-01-18 10:04:30 +0530

    solved issue #56

--- src/Darkmode/Darkmode.jsx
+++ src/Darkmode/Darkmode.jsx
@@ -1,28 +1,34 @@
-
-import  { useEffect, useState } from 'react';
-import './Darkmode.css';
+import { useEffect, useState } from "react";
+import { sun, moon } from "../assets/index.js";
+import "./Darkmode.css";
 
 const Darkmode = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const [active, setActive] = useState(true);
 
   useEffect(() => {
-    const storedDarkMode = localStorage.getItem('darkMode') === 'true';
+    const storedDarkMode = localStorage.getItem("darkMode") === "true";
     setDarkMode(storedDarkMode);
   }, []);
 
   const toggleDarkMode = () => {
     setDarkMode((prevDarkMode) => !prevDarkMode);
+    setActive(() => !active);
   };
 
   useEffect(() => {
-    document.body.classList.toggle('dark-mode', darkMode);
-    localStorage.setItem('darkMode', darkMode);
+    document.body.classList.toggle("dark-mode", darkMode);
+    localStorage.setItem("darkMode", darkMode);
   }, [darkMode]);
 
   return (
-    <div className="darkmode">
+    <div className="darkmod">
       <button onClick={toggleDarkMode} className="dark-mode-toggle">
-        Dark Mode
+        {active ? (
+          <img src={sun} alt="sun" width={"40px"} />
+        ) : (
+          <img src={moon} alt="moon" width={"40px"} />
+        )}
       </button>
       {/* Your darkmode content */}
     </div>
