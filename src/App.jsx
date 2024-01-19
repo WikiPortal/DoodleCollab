fd98c1515685d18a13c9f596b9bec8cc8e0c0cf7
AbhineshJha <abhinesh_223002@saitm.org>
2024-01-19 13:36:24 +0530

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Register from "./pages/Authentication/Register";
 import Navbar from "./constants/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Authentication/Login";
-import Footer from "./constants/Navbar/Footer";
+import Footer from "./constants/Navbar/Footer/Footer"
+
 function App() {
   const isUserLoggedIn = !!localStorage.getItem("token");
 
