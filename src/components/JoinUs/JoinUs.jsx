a5b7131ee14c565d7740bdbd881834a1e05a5974
hiCXK <hicsk24@gmail.com>
2024-01-20 16:24:26 +0530

    added JoinUs component

---
+++ src/components/JoinUs/JoinUs.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './joinus.css';
+
+const JoinUs = () => {
+    return (
+        <center><div className="invite-container">
+            <div className="invite-content">
+                <h1>Join <span className="highlight-text">DoodleCollab</span></h1>
+                <h1>discord community</h1>
+                <p>Your Ultimate Whiteboard and Sharing Solution</p>
+            </div>
+            <button className="join-button">Join Discord</button>
+        </div></center>
+    );
+}
+
+export default JoinUs;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+// import JoinUs from "src/components/JoinUs";
+import JoinUs from "../../components/JoinUs/JoinUs"
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <JoinUs/>
+    </div>
+  );
 };
 
 export default Home;
