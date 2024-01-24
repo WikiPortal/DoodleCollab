aa19107dde87fa7d004eeb8815039660ef3de0c1
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2024-01-24 21:18:14 +0530

    corrections
    #94

--- src/components/TrustDevelopers/TrustDevelopers.jsx
+++ src/components/TrustDevelopers/TrustDevelopers.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import "./trustdevelopers.css";
+import MongoDB from "../../assets/Home/MongoDB.jpg";
+import firebase from "../../assets/Home/firebase.svg";
+import mirror from "../../assets/Home/mirror.jpg";
+import keploy from "../../assets/Home/keploy.png";
+
 
 const TrustDevelopers = () => {
     return (
       <div className="trust-developers-container">
         <h5>Trusted by developers</h5>
         <div className="logo-container">
-          <img src="src/components/Images/MongoDB.jpg" alt="Logo4" />
-          <img src="src/components/Images/firebase.svg" alt="Logo2" />
-          <img src="src/components/Images/mirror.jpg" alt="Logo3" />
-          <img src="src/components/Images/keploy.png" alt="Logo1" />
+          <img src={MongoDB} alt="Logo1" />
+          <img src={firebase} alt="Logo2" />
+          <img src={mirror} alt="Logo3" />
+          <img src={keploy} alt="Logo4" />
         </div>
         <br></br>
       </div>
