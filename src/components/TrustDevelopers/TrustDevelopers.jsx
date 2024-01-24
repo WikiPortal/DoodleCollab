4fbff6985be17c55a96fc99def3d5e162b3595fe
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2024-01-24 19:53:34 +0530

    Trust Developer page added
    closes #94

---
+++ src/components/TrustDevelopers/TrustDevelopers.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./trustdevelopers.css";
+
+const TrustDevelopers = () => {
+    return (
+      <div className="trust-developers-container">
+        <h5>Trusted by developers</h5>
+        <div className="logo-container">
+          <img src="src/components/Images/MongoDB.jpg" alt="Logo4" />
+          <img src="src/components/Images/firebase.svg" alt="Logo2" />
+          <img src="src/components/Images/mirror.jpg" alt="Logo3" />
+          <img src="src/components/Images/keploy.png" alt="Logo1" />
+        </div>
+        <br></br>
+      </div>
+    );
+  };
+  
+  export default TrustDevelopers;
