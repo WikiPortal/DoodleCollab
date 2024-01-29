0c02f1192e8455296aa99c561cba67df75ae23ce
NUHASHROXME <106915718+YashNuhash@users.noreply.github.com>
2024-01-29 07:43:20 +0000

    Resolve conflicts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,15 @@
 import React from "react";
-import { useState } from "react";
-
-import App from "../../App";
-import Faq from "../../components/Faq/Faq";
-import HomeAbout from "../../components/About/HomeAbout";
-import JoinUs from "../../components/JoinUs/JoinUs";
-import HomeBanner from "../../components/Banner/HomeBanner";
-import FooterMenu from "../../components/NavFooter/FooterMenu";
-
 import { useTheme } from "../../context/ThemeContext";
 import "./home.css";
+import HomeBanner from "../../components/Banner/HomeBanner";
 import TrustDevelopers from "../../components/TrustDevelopers/TrustDevelopers";
+import HomeAbout from "../../components/About/HomeAbout";
+import JoinUs from "../../components/JoinUs/JoinUs";
+import Faq from "../../components/Faq/Faq";
+import FooterMenu from "../../components/NavFooter/FooterMenu";
 
-<<<<<<< HEAD
 const Home = ({ isBarsClicked, handleBarsClick }) => {
-
-  const { isDarkMode, toggleDarkMode } = useTheme();
-=======
-const Home = () => {
   const { isDarkMode } = useTheme();
->>>>>>> e5fecd22f791c6da410d4da01a830a01dfd552fb
-
 
   return (
     <section
