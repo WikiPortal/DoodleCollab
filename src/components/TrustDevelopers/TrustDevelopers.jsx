f6002bdfdd4d18ef15633fab03f673b6c7c7d456
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2024-01-29 20:44:10 +0530

    corrections

--- src/components/TrustDevelopers/TrustDevelopers.jsx
+++ src/components/TrustDevelopers/TrustDevelopers.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./trustdevelopers.css";
-import MongoDB from "../../assets/Home/MongoDB.jpg";
+import MongoDB from "../../assets/Home/MongoDB.png";
 import firebase from "../../assets/Home/firebase.svg";
-import mirror from "../../assets/Home/mirror.jpg";
+import mirror from "../../assets/Home/mirror.png";
 import keploy from "../../assets/Home/keploy.png";
 
 
@@ -11,10 +11,10 @@ const TrustDevelopers = () => {
       <div className="trust-developers-container">
         <h5>Trusted by developers</h5>
         <div className="logo-container">
-          <img src={MongoDB} alt="Logo1" />
-          <img src={firebase} alt="Logo2" />
-          <img src={mirror} alt="Logo3" />
-          <img src={keploy} alt="Logo4" />
+          <img className="logo-container1 "src={MongoDB} alt="Logo1" />
+          <img className="logo-container2" src={firebase} alt="Logo2" />
+          <img className="logo-container3" src={mirror} alt="Logo3" />
+          <img className="logo-container4" src={keploy} alt="Logo4" />
         </div>
         <br></br>
       </div>
