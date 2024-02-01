a290fc3ef832cc939a09ca20c5ce2bcbc0823d04
AryanKuAg <alemantrixaryan@gmail.com>
2024-02-01 09:24:41 +0530

    blog page bg color modified

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import blogData from "./blogData.json"
 import { useTheme } from "../../context/ThemeContext";
 import BlogCard from '../../components/Blog/BlogCard';
-import "./blog.css"
 
 export default function Blog() {
 
     const { isDarkMode } = useTheme();
 
     return (
-        <div className={`flex flex-col items-center mt-[90px] pb-20 pt-6 ${isDarkMode? "blog-section-dark-mode" : "blog-section-white-mode"}`}>
+        <div className={`flex flex-col items-center mt-[90px] pb-20 pt-6`} style={isDarkMode? {backgroundColor: "#1a1a1a",
+            color: "#ffffff"}: {backgroundImage: 'linear-gradient(rgb(255, 255, 255), rgb(241, 234, 250) 48%, rgb(255, 255, 255))'}}>
             <div className={`border border-gray-200 py-2 px-6 text-gray-600 rounded-full text-sm font-semibold ${!isDarkMode ? "bg-white" :"bg-black"}`}>
                 Our Blogs
             </div>
