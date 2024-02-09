acf3eb080c6a0e336e19c67a5b934b472578a1e3
AryanKuAg <alemantrixaryan@gmail.com>
2024-02-09 23:23:23 +0530

    converted all footer css to tailwindcss

--- src/constants/Footer/Footer.jsx
+++ src/constants/Footer/Footer.jsx
@@ -3,7 +3,6 @@ import { FaDiscord, FaGithub } from "react-icons/fa";
 import { FaDribbble } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
-import "./footer.css";
 import { Link } from "react-router-dom";
 import { useTheme } from "../../context/ThemeContext";
 
@@ -11,20 +10,20 @@ const Footer = () => {
   const { isDarkMode } = useTheme();
 
   return (
-    <section className={`footer ${isDarkMode ? "dark-mode" : "white-mode"}`}>
-      <div className="footer-icons">
-        <ul>
-          <li>
+    <section className={`p-0 flex flex-col items-center py-4 ${isDarkMode ? "bg-[#333] text-white" : ""}`}>
+      <div className="flex items-center w-[303px] justify-center">
+        <ul className="list-none p-0 m-0 flex cursor-pointer">
+          <li className="mx-[10px]">
             <Link
               to="https://github.com/WikiPortal/DoodleCollab"
               target="_blank"
               aria-label="gitHub link"
               rel="noopener noreferrer"
             >
-              <FaGithub />
+              <FaGithub className="h-5 w-5"/>
             </Link>
           </li>
-          <li>
+          <li className="mx-[10px]">
             <Link
               to="https://discord.com/invite/Ar84xkXkZt"
               target="_blank
@@ -32,10 +31,10 @@ const Footer = () => {
               aria-label="discord link"
               rel="noopener noreferrer"
             >
-              <FaDiscord />
+              <FaDiscord className="h-5 w-5"/>
             </Link>
           </li>
-          <li>
+          <li className="mx-[10px]">
             <Link
               to="https://doodlecollab.vercel.app/"
               target="_blank
@@ -43,10 +42,10 @@ const Footer = () => {
             aria-label="dribble link"  
             rel="noopener noreferrer"
             >
-              <FaDribbble />
+              <FaDribbble className="h-5 w-5"/>
             </Link>
           </li>
-          <li>
+          <li className="mx-[10px]">
             <Link
               to="https://twitter.com/itsmeroy69"
               target="_blank
@@ -54,10 +53,10 @@ const Footer = () => {
               aria-label="twitter link"
               rel="noopener noreferrer"
             >
-              <FaTwitter />
+              <FaTwitter className="h-5 w-5"/>
             </Link>
           </li>
-          <li>
+          <li className="mx-[10px]">
             <Link
               to="https://www.linkedin.com/in/jyotirmoyroy69/"
               target="_blank
@@ -65,12 +64,12 @@ const Footer = () => {
               aria-label="linkedin link"
               rel="noopener noreferrer"
             >
-              <FaLinkedinIn />
+              <FaLinkedinIn className="h-5 w-5"/>
             </Link>
           </li>
         </ul>
       </div>
-      <div className="footer-content">
+      <div className="flex flex-col items-end font-[600] text-lg">
         <p className="text-xs md:text-sm mt-2">
           Designed & Developed by WikiPortal
         </p>
