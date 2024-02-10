9e203fb5d407c16e782e676e7b92a0bc80dafb18
AryanKuAg <alemantrixaryan@gmail.com>
2024-02-10 12:11:18 +0530

    trust developers page converted

--- src/components/TrustDevelopers/TrustDevelopers.jsx
+++ src/components/TrustDevelopers/TrustDevelopers.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import "./trustdevelopers.css";
 import MongoDB from "../../assets/Home/MongoDB.png";
 import firebase from "../../assets/Home/firebase.svg";
 import mirror from "../../assets/Home/mirror.png";
 import keploy from "../../assets/Home/keploy.png";
 
-
 const TrustDevelopers = () => {
     return (
-      <section className="trust-developers-section">
-        <h2>Trusted & Built by Developers</h2>
-        <div className="logo-container">
-          <img src={MongoDB} alt="Logo1" />
-          <img src={firebase} alt="Logo2" />
-          <img src={mirror} alt="Logo3" />
-          <img src={keploy} alt="Logo4" />
+      <section className="text-center m-11">
+        <h2 className="text-[#3a91dc]">Trusted & Built by Developers</h2>
+        <div className="flex justify-center items-center">
+          <img src={MongoDB} alt="Logo1" className="w-[100px] my-[15px] mx-[55px] hover:transition-all hover:duration-[0.6s] hover:ease-[ease-in-out] hover:scale-[1.2] transition-all duration-[0.6s] ease-[ease-in-out] scale-100"/>
+          <img src={firebase} alt="Logo2" className="w-[100px] my-[15px] mx-[55px] hover:transition-all hover:duration-[0.6s] hover:ease-[ease-in-out] hover:scale-[1.2] transition-all duration-[0.6s] ease-[ease-in-out] scale-100"/>
+          <img src={mirror} alt="Logo3" className="w-[100px] my-[15px] mx-[55px] hover:transition-all hover:duration-[0.6s] hover:ease-[ease-in-out] hover:scale-[1.2] transition-all duration-[0.6s] ease-[ease-in-out] scale-100"/>
+          <img src={keploy} alt="Logo4" className="w-[100px] my-[15px] mx-[55px] hover:transition-all hover:duration-[0.6s] hover:ease-[ease-in-out] hover:scale-[1.2] transition-all duration-[0.6s] ease-[ease-in-out] scale-100"/>
         </div>
       </section>
     );
